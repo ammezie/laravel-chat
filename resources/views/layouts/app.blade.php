17b767315fd1dc8bd2b34d53a93f84af53c33099
Jeremy <jeremykenedy@gmail.com>
2020-05-24 04:24:05 -0700

    update assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <link href="/css/app.css" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <style>
         .chat {
             list-style: none;
@@ -113,6 +113,6 @@
         @yield('content')
 
     </div>
-    <script src="/js/app.js"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
